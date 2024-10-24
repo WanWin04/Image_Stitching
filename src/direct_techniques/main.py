308b3578d05a9796c8045333d97ea17e4b34b09b
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-10-25 00:10:14 +0700

    Direct method -  Gradient-domain Image Stitching of Levin et al

--- src/direct_techniques/main.py
+++ src/direct_techniques/main.py
@@ -3,42 +3,47 @@ import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-def compute_initial_homography(img1, img2):
-    h, w = img1.shape[:2]
-    points1 = np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], dtype='float32')
-    points2 = np.array([[0, 0], [w-1, 0], [w-1, h-1], [0, h-1]], dtype='float32')
-    
-    H, status = cv2.findHomography(points1, points2)
-    return H
+def compute_gradients(img):
+    grad_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+    grad_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    return grad_x, grad_y
 
-def warp_image(img, H, shape):
-    return cv2.warpPerspective(img, H, shape)
-
-def objective_function(H, img1, img2):
+def feathered_cost_function(H, img1, img2):
     H = H.reshape(3, 3)
-    warped_img2 = warp_image(img2, H, (img1.shape[1], img1.shape[0]))
+    warped_img2 = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]))
+
+    grad1_x, grad1_y = compute_gradients(img1)
+    grad2_x, grad2_y = compute_gradients(warped_img2)
+
+    cost = np.sum(np.abs(grad1_x - grad2_x)) + np.sum(np.abs(grad1_y - grad2_y))
     
-    return np.sum((img1 - warped_img2) ** 2)
+    return cost
 
 def optimize_homography(img1, img2, H_init):
-    result = minimize(objective_function, H_init.flatten(), args=(img1, img2), method='L-BFGS-B')
+    result = minimize(feathered_cost_function, H_init.flatten(), args=(img1, img2), method='L-BFGS-B')
     return result.x.reshape(3, 3)
 
+def poisson_blend(img1, img2, mask):
+    center = (img1.shape[1] // 2, img1.shape[0] // 2)
+    blended = cv2.seamlessClone(img2, img1, mask, center, cv2.NORMAL_CLONE)
+    return blended
+
 def blend_images(img1, img2, H):
-    warped_img2 = warp_image(img2, H, (img1.shape[1], img1.shape[0]))
-    
+    warped_img2 = cv2.warpPerspective(img2, H, (img1.shape[1], img1.shape[0]))
+
     mask = (warped_img2 > 0).astype(np.uint8) * 255
-    blended = cv2.seamlessClone(warped_img2, img1, mask, (img1.shape[1]//2, img1.shape[0]//2), cv2.NORMAL_CLONE)
+
+    blended = poisson_blend(img1, warped_img2, mask)
     
     return blended
 
 def stitch_images(img1, img2):
-    H_init = compute_initial_homography(img1, img2)
-    
+    H_init = np.eye(3)
+
     H_optimized = optimize_homography(img1, img2, H_init)
-    
+
     blended_image = blend_images(img1, img2, H_optimized)
-    
+
     return blended_image, H_optimized
 
 img1 = cv2.imread('../../image/test1.jpg')
@@ -51,6 +56,8 @@ result, homography = stitch_images(img1, img2)
 
 plt.figure(figsize=(10, 5))
 plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-plt.title('Stitched Image')
+plt.title('Stitched Image using Gradient-domain Image Stitching')
 plt.axis('off')
 plt.show()
+
+# https://link.springer.com/chapter/10.1007/978-3-540-24673-2_31?utm_source=getftr&utm_medium=getftr&utm_campaign=getftr_pilot&getft_integrator=sciencedirect_contenthosting
